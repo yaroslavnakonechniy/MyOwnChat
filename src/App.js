5d9884aad7d92172c270658c5f1e8e3ceeec9fc8
yaroslavn <yaroslav.n@open-tag.com>
2023-06-15 12:24:25 +0300

    With Router: created a method that receives the last url parameter and displays the corresponding user.

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = (props) => {
         <Navbar />
         <div className="app-wrapper-content">
           <Routes>
-            <Route path='/profile' element={<ProfileContainer />}/>
+            <Route path='/profile/:userId' element={<ProfileContainer />}/>
             <Route path='/dialogs' element={<DialogsContainer />}/>
             <Route path='/user' element={<UserContainer />}/>
             <Route path='/messages' element={<Messages />}/>
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,18 +3,24 @@ import Profile from "./Profile";
 import axios from "axios";
 import {connect} from "react-redux";
 import {setUserProfile} from "../../redux/post-reducer";
+import {useLocation,useNavigate,useParams} from "react-router-dom";
 
 class ProfileContainer extends React.Component {
 
   componentDidMount(){
+    debugger
+    let userId = this.props.router.params.userId;
+    console.log(userId);
+    debugger
+    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/`+ userId)
     
-    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
     .then (response => {
       debugger
         
         this.props.setUserProfile(response.data);
         
     });
+    debugger
 }
 
   render(){
@@ -22,14 +28,25 @@ class ProfileContainer extends React.Component {
     return  <Profile  {...this.props} profile = {this.props.profile} />
   }
 
-  
-
-      
-    
 }
  let mapStateToProps = (state) => ({
   profile: state.PostPage.profile
  })
 
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+      let location = useLocation();
+      let navigate = useNavigate();
+      let params = useParams();
+      return (
+          <Component
+              {...props}
+              router={{ location, navigate, params }}
+          />
+      );
+  }
 
-export default connect(mapStateToProps,{setUserProfile} ) (ProfileContainer);
+  return ComponentWithRouterProp;
+}
+
+export default connect(mapStateToProps, {setUserProfile})(withRouter(ProfileContainer));
