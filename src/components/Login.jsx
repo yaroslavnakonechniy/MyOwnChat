8b9c0f0b2161c44d47da56fb07aaa00223790f94
yaroslavn <yaroslav.n@open-tag.com>
2023-07-12 13:35:12 +0300

    Form for login: create form for login.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import {Field, reduxForm } from 'redux-form';
+import { Input } from "./common/FormsControls/FormsControls";
+import {required} from "../utils/validators/validators";
 
-let Login = (props) => {
-    return <h1>Loginnnn? залогінся або зміни поле isAuth на true в src\redux\auth-reducer.js</h1>
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"Login"} name={"login"} component={Input} validate={[required]}/>
+            </div>
+            <div>
+                <Field placeholder={"Password"}name={"password"} component={Input} validate={[required]}/>
+            </div>
+            <div>
+                <Field component={Input} name={"rememberMe"} type={"checkbox"} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm ({
+    form: 'login'
+})(LoginForm)
+
+
+
+const Login = (props) => {
+
+    const onSubmit = (FormData) => {
+        console.log(FormData);
+    }
+
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm  onSubmit={onSubmit} />
+    </div>
 }
 
 export default Login;
