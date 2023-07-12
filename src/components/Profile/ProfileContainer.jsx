26f17672caaef8a65affbd4b53a0e4d2d4cd03c6
yaroslavn <yaroslav.n@open-tag.com>
2023-07-12 13:38:46 +0300

    Form for login: deleted debugger for ProfileContainer.

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -18,7 +18,6 @@ class ProfileContainer extends React.Component {
   }
 
   render(){
-    debugger
     return  <Profile  {...this.props} profile = {this.props.profile} status = {this.props.status} updateStatus ={this.props.updateStatus}/>
   }
 
